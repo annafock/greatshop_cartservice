b9fe27f98b2e2066451710505b4a05b031d7b93c
Anna Fock <anna@fock.se>
2018-03-02 16:10:46 +0100

    add class CartBuilder and code for App class

--- src/main/java/com/cart/App.java
+++ src/main/java/com/cart/App.java
@@ -1,13 +1,36 @@
 package com.cart;
 
-/**
- * Hello world!
- *
- */
+import java.util.Date;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Link;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+@ApplicationPath("/cart")
+@Path("")
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+	@GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getDirectory(@Context UriInfo uri) {
+        Link selfLink = Link.fromUri(uri.getBaseUri())
+                .rel("self").type(MediaType.APPLICATION_JSON)
+                .build();
+        Link productsLink = Link.fromUri(uri.getBaseUri() + "cart")
+                .rel("cart").type(MediaType.APPLICATION_JSON)
+                .build();
+
+        return Response.ok()
+                .lastModified(new Date())
+                .location(uri.getRequestUri())
+                .links(selfLink, productsLink)
+                .build();
     }
 }
